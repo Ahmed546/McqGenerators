bf46300c493a1f16d230fbae82cdd1ccf89e74ab
Ahmed546 <ahmedwaleed0546@gmail.com>
2024-11-27 22:00:29 +0500

    code updated

--- setup.py
+++ setup.py
@@ -6,8 +6,5 @@ setup(
     author='waleed ahmed',
     author_email='ahmedwaleed0546@gmail.com',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'openai',"langchain","streamlit","python-dotenv","pyPDF2"],
+
     description='An AI mcq generator package',)
